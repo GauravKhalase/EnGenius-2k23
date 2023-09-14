a8a401522c1308e10bc839e82f7cf191ce1f586e
GauravKhalase <gauravkhalase@gmail.com>
2023-09-14 17:35:47 +0530

    Warning added

--- src/components/Pass.jsx
+++ src/components/Pass.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import warning from "../assets/warning.jpeg"
 
 const Pass = () => {
   return (
@@ -16,23 +17,50 @@ const Pass = () => {
           innovation, and celebration of Engineers' Day.
         </div>
         <div className="text-center pb-5">
-          <div className="font-semibold">The Pass includes some benefits mentioned...</div>
+          <div className="font-semibold">
+            The Pass includes some benefits mentioned...
+          </div>
           <div className="text-[15px]">
             1. Enjoy the event show with comfort and reserved seat for oneself.
           </div>
-          <div className="text-[15px]">2. Snacks will be provided during the break.</div>
-        </div >
-        <div className="text-center  pb-5">This PASS costs you only <span className="font-semibold">30/-</span> per head for entire Event</div>
-        <div className="text-red-500 font-bold text-xl uppercase pb-5">Sorry! <span className="text-[15px] font-semibold text-black">you are late.</span></div>
-        <div className="text-center font-semibold pb-5">All seats are fully booked for EnGenius-2023.</div>
-        
+          <div className="text-[15px]">
+            2. Snacks will be provided during the break.
+          </div>
+        </div>
+        <div className="text-center  pb-5">
+          This PASS costs you only <span className="font-semibold">30/-</span>{" "}
+          per head for entire Event
+        </div>
+        <div className="text-red-500 font-bold text-xl uppercase pb-5">
+          Sorry!{" "}
+          <span className="text-[15px] font-semibold text-black">
+            you are late.
+          </span>
+        </div>
+        <div className="text-center font-semibold pb-6">
+          All seats are fully booked for EnGenius-2023.
+        </div>
+
+        <div className=" text-center  ">
+          <img className="w-[100px] mx-auto pb-5" src={warning} alt="" />
+          <div span className="text-red-500 font-bold text-xl uppercase pb-4">
+            Attention Please
+          </div>
+          <div className="text-[18px] font-semibold pb-5">
+            Please don't pay any amount without google form otherwise amount
+            will not be refundable.
+          </div>
+        </div>
         <a className="pb-5" href="">
           {/* bg-gradient-to-r from-[#4327a3] to-[#dd117f] */}
-          <button className=" text-[12px] cursor-default font-semibold text-black bg-gray-400 tracking-wide  rounded-xl px-6 py-3">
+          {/* <button className=" text-[12px] cursor-default font-semibold text-black bg-gray-400 tracking-wide  rounded-xl px-6 py-3">
             GET THE PASS NOW*
-          </button>
+          </button> */}
         </a>
-        <div className="text-center"><span className="font-bold">NOTE : </span>Keep your entry pass with you for venue entry.</div>
+        <div className="text-center">
+          <span className="font-bold">NOTE : </span>Keep your entry pass with
+          you for venue entry.
+        </div>
       </div>
     </div>
   );
